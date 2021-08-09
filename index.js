f7848f040ae1f8d6df32fb420c89e7e547e82d48
unknown <x1tmwz@gmail.com>
2021-08-09 20:02:10 +0300

    last time fix server static serve

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const config = require('config');
 const app = require('./src/app');
-const path = require('path')
 const port = process.env.PORT || config.get("port");
 
-const publicPath = path.join(__dirname, './client','build');
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
-});
 
 app.listen(port, function(){
     console.log("server start on port " + port);
--- src/app.js
+++ src/app.js
@@ -4,14 +4,25 @@ const userRouter = require("./routers/user");
 const workerRouter = require("./routers/worker");
 const route404 = require('./middleware/route404');
 const apiErrorHandler = require('./middleware/apiErrorHandler');
+const path = require('path')
 
 
 
 const app = express();
 
+
+const publicPath = path.join(__dirname, '../client','build');
+
+
+app.use(express.static(publicPath));
+
 app.use(express.json());
 app.use(userRouter);
 app.use(workerRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 app.use(route404);
 app.use(apiErrorHandler);
