4df11c7d9e4d07c200276fa2c223fb418259da16
unknown <x1tmwz@gmail.com>
2021-08-09 19:41:48 +0300

    change the folder of the static serve in the server

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ const express = require('express');
 require('./db/mongoose');
 const userRouter = require("./routers/user");
 const workerRouter = require("./routers/worker");
-const path = require('path')
 const route404 = require('./middleware/route404');
 const apiErrorHandler = require('./middleware/apiErrorHandler');
 
-const publicPath = path.join(__dirname, './client','build');
+
+
 const app = express();
 
 app.use(express.json());
@@ -15,8 +15,6 @@ app.use(workerRouter);
 
 app.use(route404);
 app.use(apiErrorHandler);
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
+
 
 module.exports = app;
